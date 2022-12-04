9741a6db5639d33592ab07f158e310b6a0f2a013
Kr4pper <Kr4pper@github.com>
2022-12-04 11:16:23 +0100

    switch to regex implementation

--- src/ts/2022/04.ts
+++ src/ts/2022/04.ts
@@ -1,7 +1,7 @@
 export default (rawInput: string): [number?, number?] => {
     const parseAssignment = (line: string) => {
-        const [a, b] = line.split(',');
-        return [...a.split('-'), ...b.split('-')].map(a => parseInt(a));
+        const matches = line.match(/^(\d+)-(\d+),(\d+)-(\d+)/);
+        return [matches[1], matches[2], matches[3], matches[4]].map(a => parseInt(a));
     };
 
     const assignments = rawInput.split('\n').map(parseAssignment);
