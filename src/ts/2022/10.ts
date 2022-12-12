5f8e3b2bddb7e5665e917fee965e8605da1c8a3f
Kr4pper <Kr4pper@github.com>
2022-12-12 16:29:19 +0100

    solved 2022 day 10

---
+++ src/ts/2022/10.ts
@@ -0,0 +1,40 @@
+import {range} from '../utils';
+
+export default (rawInput: string): [(number | string)?, (number | string)?] => {
+    // PART 1
+    let sum = 1;
+    let cycle = 1;
+    let valueAt = new Map<number, number>();
+    for (const line of rawInput.split('\n')) {
+        const match = line.match(/(-?\d+)/);
+        const add = match?.[1];
+        if (add) {
+            valueAt.set(cycle++, sum);
+            valueAt.set(cycle++, sum);
+            sum += +add;
+            continue;
+        }
+        valueAt.set(cycle++, sum);
+    }
+
+    // PART 2
+    let row = '';
+    const crt: string[] = [];
+    for (let cycle = 1; cycle <= valueAt.size; cycle++) {
+        const x = cycle % 40;
+
+        if (x > valueAt.get(cycle) + 2 || x < valueAt.get(cycle)) row += '.';
+        else row += '#';
+
+        if (x === 0) {
+            crt.push(row);
+            row = '';
+        }
+    }
+    crt.push(row);
+
+    return [
+        range(0, Math.floor(valueAt.size / 40) - 1).map(v => 20 + 40 * v).reduce((acc, i) => acc + i * valueAt.get(i), 0),
+        [...valueAt.values()].reduce((acc, v, idx) => acc + v * idx, 0), // visual output is ZFBFHGUP, using this for testing
+    ];
+};
