e6d22d0156932dc930ab0fa70b09bc5b6c5a9041
Kr4pper <Kr4pper@github.com>
2024-12-03 13:49:52 +0100

    solved 2024 day 3

---
+++ src/ts/2024/03.ts
@@ -0,0 +1,32 @@
+import {Hashtable, sum} from '../utils';
+
+export default (input: string): [(number | string)?, (number | string)?] => {
+    // PART 1
+    const executeMUL = (instruction: string) => {
+        const [_, a, b] = instruction.match(/(\d{1,3}),(\d{1,3})/);
+        return +a * +b;
+    };
+
+    const parse = (input: string, ...instructions: string[]) =>
+        input.match(new RegExp(instructions.join('|'), 'g'));
+
+    const MUL = '(mul\\(\\d{1,3},\\d{1,3}\\))';
+
+    // PART 2
+    const DO = '(do\\(\\))';
+    const DONT = '(don\'t\\(\\))';
+    const instructions2 = parse(input, MUL, DO, DONT);
+
+    let part2 = 0;
+    let enabled = true;
+    for (const instruction of instructions2) {
+        if (instruction.startsWith("don't")) enabled = false;
+        else if (instruction.startsWith('do')) enabled = true;
+        else if (enabled) part2 += executeMUL(instruction);
+    };
+
+    return [
+        sum(parse(input, MUL).map(executeMUL)),
+        part2,
+    ];
+};
--- src/ts/utils/arithmetic.ts
+++ src/ts/utils/arithmetic.ts
@@ -5,3 +5,5 @@ export const subtract = (a: number, b: number) => a - b;
 export const multiply = (a: number, b: number) => a * b;
 
 export const divide = (a: number, b: number) => a / b;
+
+export const sum = (ns: number[]) => ns.reduce((sum, n) => sum + n, 0);
