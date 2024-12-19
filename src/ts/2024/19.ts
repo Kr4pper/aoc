e3abac9b66fc66e49527857b32c50c6d22323ba6
Kr4pper <Kr4pper@github.com>
2024-12-19 15:56:50 +0100

    2024 day 19 performance improvement

--- src/ts/2024/19.ts
+++ src/ts/2024/19.ts
@@ -1,22 +1,12 @@
 import {memoize} from '../utils';
 
 export default (rawInput: string): [(number | string)?, (number | string)?] => {
-    // PART 1
     const parse = (input: string) => {
         const [_towels, _designs] = input.split('\n\n');
         return [_towels.split(', '), _designs.split('\n')];
     };
     const [towels, designs] = parse(rawInput);
 
-    const isPossible = memoize((design: string): boolean =>
-        design.length === 0
-            ? true
-            : towels.filter(t => design.startsWith(t))
-                .map(t => design.slice(t.length))
-                .some(isPossible)
-    );
-
-    // PART 2
     const countPossible = memoize((design: string): number =>
         design.length === 0
             ? 1
@@ -25,8 +15,9 @@ export default (rawInput: string): [(number | string)?, (number | string)?] => {
                 .reduce((sum, d) => sum + countPossible(d), 0)
     );
 
+    const counts = designs.map(countPossible);
     return [
-        designs.filter(isPossible).length,
-        designs.map(countPossible).reduce((sum, count) => sum + count, 0),
+        counts.filter(v => v > 0).length,
+        counts.reduce((sum, count) => sum + count, 0),
     ];
 };
