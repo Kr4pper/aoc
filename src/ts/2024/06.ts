77fbb1048d0e352574c2f20f7215c73e19a5d9d3
Kr4pper <Kr4pper@github.com>
2024-12-24 16:49:54 +0100

    wip

--- src/ts/2024/06.ts
+++ src/ts/2024/06.ts
@@ -6,6 +6,7 @@ export default (rawInput: string): [(number | string)?, (number | string)?] => {
     // PART 1
     const map = Grid2D.parse(input);
     const [startX, startY] = map.findValue('^');
+    map.set(startX, startY, '.');
 
     const DIRECTIONS = [
         [0, -1],
@@ -15,63 +16,92 @@ export default (rawInput: string): [(number | string)?, (number | string)?] => {
     ];
 
     const visited = new Set<string>();
+    const jumps: [x: number, y: number, heading: number][] = [];
     let [x, y] = [startX, startY];
     let heading = 0;
     while (true) {
         visited.add(`${x},${y}`);
         const [dx, dy] = DIRECTIONS[heading];
         const next = map.get(x + dx, y + dy);
-        if (next === '.' || next === '^') {
+        if (next === '.') {
             x += dx;
             y += dy;
         }
         else if (next === '#') {
+            jumps.push([x, y, heading]);
             heading = (heading + 1) % 4;
         }
         else break; // out of bounds
     }
 
     // PART 2
-    const mapHasLoop = (grid: Grid2D<string>) => {
-        const visitedObstacles = new Set<string>();
+    const isBetween = (a: number, b: number, test: number) => {
+        const min = Math.min(a, b);
+        const max = Math.max(a, b);
+        return test >= min && test <= max;
+    };
+
+    let steps = 0;
+    const mapHasLoop = (grid: Grid2D<string>, obstacleX: number, obstacleY: number) => {
+        const previousTurns = new Set<string>();
         let [x, y] = [startX, startY];
         let heading = 0;
+        let jumpIdx = 0;
+        /*
+        while (true) { // advance until jumps are interrupted by new obstacle
+            steps++;
+            const [nextX, nextY, nextHeading] = jumps[jumpIdx++];
+            if ((x === nextX && isBetween(y, nextY, obstacleY))
+                || (y === nextY && isBetween(x, nextX, obstacleX))) {
+                break;
+            }
+            previousTurns.add(`${x},${y},${heading}`);
+            [x, y] = [nextX, nextY];
+            heading = nextHeading;
+        }
+        */
         while (true) {
+            steps++;
             const [dx, dy] = DIRECTIONS[heading];
             const next = grid.get(x + dx, y + dy);
-            if (next === '.' || next === '^') {
+            if (next === '.') {
                 x += dx;
                 y += dy;
             }
             else if (next === '#') {
                 const movement = `${x},${y},${heading}`;
-                if (visitedObstacles.has(movement)) {
+                /*
+                const jump = jumps.findIndex(([_x, _y, _heading]) => x === _x && y === _y && heading === _heading);
+                if (jump > -1) {
+                    //console.log(jump, jumps[jump]);
+                    return true;
+                }
+                    */
+                if (previousTurns.has(movement)) {
                     return true;
                 }
 
-                visitedObstacles.add(movement);
+                previousTurns.add(movement);
                 heading = (heading + 1) % 4;
             }
-            else {
-                return false; // out of bounds
-            }
+            else return false; // out of bounds
         }
     };
 
-    let potentialLoops = 0;
+    let loops = 0;
     for (let x = 0; x < map.width; x++) {
         for (let y = 0; y < map.height; y++) {
             if (map.get(x, y) !== '.') continue;
-            if (!visited.has(`${x},${y}`)) continue;
+            if (!visited.has(`${x},${y}`)) continue; // not on original path
 
             map.set(x, y, '#');
-            if (mapHasLoop(map)) potentialLoops++;
+            if (mapHasLoop(map, x, y)) loops++;
             map.set(x, y, '.');
         }
     }
 
     return [
         visited.size,
-        potentialLoops,
+        loops,
     ];
 };
